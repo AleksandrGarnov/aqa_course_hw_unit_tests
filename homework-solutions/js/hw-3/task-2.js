bac67919a95f48f0f04a54dec0ac50b6250da855
Aleksandr <a.garnov@paysenger.com>
2025-02-12 10:21:56 +0400

    added second task

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -4,3 +4,14 @@
 n + nn + nnn, где n не перемножаются, а конкатенируются
 
 */
+let firstNumber = 3;
+let resultNumber1 = String (firstNumber);
+let doubleconcatenation = +(firstNumber + resultNumber1)
+let tripleconcatenation = +(firstNumber + resultNumber1 + resultNumber1)
+
+//первый вариант вывода
+console.log(firstNumber + doubleconcatenation + tripleconcatenation)
+//второй вариант вывода
+console.log(firstNumber + +(firstNumber+resultNumber1) + +(firstNumber + resultNumber1 + resultNumber1))
+// третий вариант вывода
+console.log(firstNumber + Number(firstNumber+resultNumber1) + Number(firstNumber + resultNumber1 + resultNumber1))
