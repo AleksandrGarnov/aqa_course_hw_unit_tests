e46e3460c130b1e379d7f47321b0fa56f4e5625f
Aleksandr <a.garnov@paysenger.com>
2025-02-15 00:00:07 +0400

    added task-2

--- homework-solutions/js/hw-4/task-2.js
+++ homework-solutions/js/hw-4/task-2.js
@@ -14,3 +14,20 @@
   5. Проверить задание со следующими значениями в переменной age: 10, 17, 18, 19, 59, 60, 61
 
   */
+
+  let minAge = 18;
+  let maxAge = 60;
+
+  let age
+  age = 61
+  if (typeof age != "number"){
+    console.log ("Incorrect data type")
+  } else if(age < minAge) {
+    console.log ("You don't have access cause your age is " + age + ". It's less then " + minAge)
+  } else if (age >= minAge && age <= maxAge) { // я добавил условие age <= maxAge иначе у нас неучтенный кейс с age = 60. Если бы не поставил, нас бы кинуло на Technical work
+    console.log ("Welcome!")
+  } else if (age > maxAge) {
+    console.log ("Keep calm and look Culture channel")
+  } else {
+    console.log ("Technical work")
+  }
