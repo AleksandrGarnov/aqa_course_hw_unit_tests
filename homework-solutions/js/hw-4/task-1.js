5310ca09c98e0a45c945ee0e4f681fd9b9cae514
Aleksandr <a.garnov@paysenger.com>
2025-02-14 23:25:57 +0400

    add 1st task in hw-4

--- homework-solutions/js/hw-4/task-1.js
+++ homework-solutions/js/hw-4/task-1.js
@@ -3,3 +3,20 @@
  - Создайте переменную salary со значением 1000
   - Создайте переменную grade, которая должна получить значение "middle" если salary больше или равна 1000, и значение "junior" - если меньше
   */
+//первый вариант
+
+  let salary = 100000;
+  let grade
+  if (salary >= 1000) {
+    grade = "middle";
+  } else {
+     grade = "junior";
+  }
+
+  console.log(grade);
+
+
+  //второй вариант
+ let salary2 = 100;
+ let grade2 = salary2 >= 1000 ? "middle" : "junior";
+    console.log(grade2);
