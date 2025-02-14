bb80b9633ac38404c535159f016e9da5315ec0ac
Aleksandr <a.garnov@paysenger.com>
2025-02-14 23:25:57 +0400

    add 1st task in hw-4

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -3,15 +3,22 @@
 Напишите программу, которая принимает целое положительное число n (одно любое число от 1 до 9), и выводит сумму равную 
 n + nn + nnn, где n не перемножаются, а конкатенируются
 
-*/
-let firstNumber = 3;
-let resultNumber1 = String (firstNumber);
-let doubleconcatenation = +(firstNumber + resultNumber1)
-let tripleconcatenation = +(firstNumber + resultNumber1 + resultNumber1)
+// */
+// let firstNumber = 3;
+// let resultNumber1 = String (firstNumber);
+// let doubleconcatenation = +(firstNumber + resultNumber1)
+// let tripleconcatenation = +(firstNumber + resultNumber1 + resultNumber1)
 
-//первый вариант вывода
-console.log(firstNumber + doubleconcatenation + tripleconcatenation)
-//второй вариант вывода
-console.log(firstNumber + +(firstNumber+resultNumber1) + +(firstNumber + resultNumber1 + resultNumber1))
-// третий вариант вывода
-console.log(firstNumber + Number(firstNumber+resultNumber1) + Number(firstNumber + resultNumber1 + resultNumber1))
+// //первый вариант вывода
+// console.log(firstNumber + doubleconcatenation + tripleconcatenation)
+// //второй вариант вывода
+// console.log(firstNumber + +(firstNumber+resultNumber1) + +(firstNumber + resultNumber1 + resultNumber1))
+// // третий вариант вывода
+// console.log(firstNumber + Number(firstNumber+resultNumber1) + Number(firstNumber + resultNumber1 + resultNumber1))
+
+
+// let n = 5;
+// let nn = Number(`${n}${n}`);
+// let nnn = Number(`${n}${n}${n}`);
+// console.log(n + nn + nnn);
+
