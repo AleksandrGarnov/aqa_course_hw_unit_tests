947e51b808ae4cb1afe3dd4b4bda47ba244bd758
Aleksandr <a.garnov@paysenger.com>
2025-02-15 14:42:15 +0400

    add notes

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -11,17 +11,19 @@ let minAge = 18;
   let maxAge = 60;
 
   let age
-  age = 'true'
+  age = '61c'
   let checkAge = +age
 
-  if (typeof checkAge != "number" || isNaN(checkAge)){
+  if (isNaN(checkAge)){
     console.log ("Incorrect data type")
-  } else if(checkAge < minAge) {
-    console.log ("You don't have access cause your age is " + checkAge + ". It's less then " + minAge)
-  } else if (checkAge >= minAge && checkAge <= maxAge) {
-    console.log ("Welcome!")
-  } else if (checkAge > maxAge) {
-    console.log ("Keep calm and look Culture channel")
   } else {
-    console.log ("Technical work")
-  }
+      if(age < minAge) {
+        console.log ("You don't have access cause your age is " + age + ". It's less then " + minAge)
+      } else if (age >= minAge && age <= maxAge) { // я добавил условие age <= maxAge иначе у нас неучтенный кейс с age = 60. Если бы не поставил, нас бы кинуло на Technical work
+        console.log ("Welcome!")
+      } else if (age > maxAge) {
+        console.log ("Keep calm and look Culture channel")
+      } else {
+        console.log ("Technical work")
+      }
+}
