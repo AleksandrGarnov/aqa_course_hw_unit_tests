47828852d6145b02b3a02293803de22bde48767b
Aleksandr <a.garnov@paysenger.com>
2025-02-20 21:35:02 +0400

    added changes to HW-5

--- homework-solutions/js/hw-5/task-3.js
+++ homework-solutions/js/hw-5/task-3.js
@@ -4,7 +4,20 @@
  * Ожидаемый результат для `hello`: "hello contains 2 vowels and 3 consonants".
  */
 const word = 'hello';
-
 let vowelsAndConsonantsResult = '';
-
+let vowels = 0;
+let consonants = 0;
+for (let i = 0; i <= word.length; i++) {
+    if (word[i] === 'a' || word[i] === 'e' || word[i] === 'i' || word[i] === 'o' || word[i] === 'u') {
+        vowels++
+    } else if (word[i] === 'b' || word[i] === 'c' || word[i] === 'd' || word[i] === 'f' || word[i] === 'g' ||
+        word[i] === 'h' || word[i] === 'j' || word[i] === 'k' || word[i] === 'l' || word[i] === 'm' ||
+        word[i] === 'n' || word[i] === 'p' || word[i] === 'q' || word[i] === 'r' || word[i] === 's' ||
+        word[i] === 't' || word[i] === 'v' || word[i] === 'w' || word[i] === 'x' || word[i] === 'y' ||
+        word[i] === 'z') {
+        consonants++
+    }
+}
+vowelsAndConsonantsResult = `${word} contains ${vowels} vowels and ${consonants} consonants`
+console.log(vowelsAndConsonantsResult)
 export { vowelsAndConsonantsResult };
