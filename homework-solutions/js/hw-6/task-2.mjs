6040abdd7df0d056bbb8220e8fa20be580bdd750
Aleksandr <a.garnov@paysenger.com>
2025-03-04 21:18:40 +0400

    added second task

--- homework-solutions/js/hw-6/task-2.mjs
+++ homework-solutions/js/hw-6/task-2.mjs
@@ -12,8 +12,26 @@
   const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
   const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
 */
-
-let resultUnique;
+const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai']
+let competitorPizzasLoverCase = competitorPizzas.map(el => el.toLowerCase())
+// console.log(competitorPizzasLoverCase)
+const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
+const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+let resultUnique = []
 let resultNull;
+for (let i = 0; i < myPizzasT1.length;i++){
+  if(!competitorPizzasLoverCase.includes(myPizzasT1[i].toLowerCase())) {
+    resultUnique.push(myPizzasT1[i])
+  }
+};
+for (let i = 0; i < myPizzasT2.length;i++){
+  if(!competitorPizzasLoverCase.includes(myPizzasT2[i].toLowerCase())) {
+    resultUnique.push(myPizzasT2[i])
+  }
+};
+resultNull = (resultUnique.length === 0) ? null : resultUnique;
 
+console.log(resultUnique)
+console.log(resultNull)
 export { resultNull, resultUnique };
+
