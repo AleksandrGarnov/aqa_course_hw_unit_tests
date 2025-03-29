e74e2d820673b166f97175e0dae7508470576226
Aleksandr <a.garnov@paysenger.com>
2025-03-29 15:14:16 +0400

    added first part for 1 task

--- homework-solutions/js/hw-11/task-1.mjs
+++ homework-solutions/js/hw-11/task-1.mjs
@@ -1,7 +1,53 @@
 class Employee {
-  // Ваш код
+  #salary
+  constructor(firstName, lastName, profession, salary) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._profession = profession;
+    this.#salary = salary;
+  }
+
+
+  get firstName() {
+    return this._firstName;
+  };
+  get lastName(){
+    return this._lastName;
+  };
+  get profession(){
+    return this._profession
+  };
+  get salary(){
+    return this.#salary
+  }
+  set firstName(value){
+    return this._firstName = value;
+  };
+  set lastName(value){
+    return this._lastName = value;
+  };
+  set profession(value){
+    return this._profession = value;
+  };
+  set salary(value){
+    if (value < 0 || typeof (value) !== 'number') {
+      throw new Error('Invalid salary');
+    } else {
+    return this.#salary = value;
+    }
+  }
+getFullName(){ 
+    return `${this.firstName} ${this.lastName}`;
+  }
 }
 
+
+const emp1 = new Employee('John', 'Doe', 'Developer', 3000);
+
+console.log(emp1.firstName); // "John"
+emp1.salary = 3100;
+console.log(emp1.salary); // 3100
+console.log(emp1.getFullName()); 
 class Company {
   // Ваш код
 }
