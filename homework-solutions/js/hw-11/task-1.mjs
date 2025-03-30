7aeb5dc62281c2f6482429fe32613164a61ab0cc
Aleksandr <a.garnov@paysenger.com>
2025-03-30 19:32:47 +0400

    add first task

--- homework-solutions/js/hw-11/task-1.mjs
+++ homework-solutions/js/hw-11/task-1.mjs
@@ -11,62 +11,87 @@ class Employee {
   get firstName() {
     return this._firstName;
   };
-  get lastName(){
+  get lastName() {
     return this._lastName;
   };
-  get profession(){
+  get profession() {
     return this._profession
   };
-  get salary(){
+  get salary() {
     return this.#salary
   }
-  set firstName(value){
+  set firstName(value) {
     return this._firstName = value;
   };
-  set lastName(value){
+  set lastName(value) {
     return this._lastName = value;
   };
-  set profession(value){
+  set profession(value) {
     return this._profession = value;
   };
-  set salary(value){
+  set salary(value) {
     if (value < 0 || typeof (value) !== 'number') {
       throw new Error('Invalid salary');
     } else {
-    return this.#salary = value;
+      return this.#salary = value;
     }
   }
-getFullName(){ 
+  getFullName() {
     return `${this.firstName} ${this.lastName}`;
   }
 }
 
 
-const emp1 = new Employee('John', 'Doe', 'Developer', 3000);
+// const emp1 = new Employee('John', 'Doe', 'Developer', 3000);
 
-console.log(emp1.firstName); // "John"
-emp1.salary = 3100;
-console.log(emp1.salary); // 3100
-console.log(emp1.getFullName()); 
+// console.log(emp1.firstName); // "John"
+// emp1.salary = 3100;
+// console.log(emp1.salary); // 3100
+// console.log(emp1.getFullName());
+
+
 class Company {
-  constructor(title,phone,address,employees = []) {
+  #employees
+  constructor(title, phone, address, employees = []) {
     this._title = title;
     this._phone = phone;
     this._address = address;
-    this._employees = employees;
+    this.#employees = [];
   }
-  get title(){
+  get title() {
     return this._title;
   };
-  get phone () {
+  get phone() {
     return this._phone;
   };
-  get address(){
+  get address() {
     return this._address;
   }
-  }
-  }
 
+  addEmployee(employee){
+    if (employee instanceof Employee) {
+      this.#employees.push(employee);
+    }else {
+      throw new Error('Invalid Employee input');
+    }
+  }
+  getEmployees(){
+    return this.#employees
+  }
+  getInfo(){
+    return `Компания: ${this.title}\nАдрес: ${this.address}\nКоличество сотрудников: ${this.#employees.length}`
+  }
 }
+
+// const company = new Company('Tech Corp', 123456, 'Main Street');
+// const emp1 = new Employee('John', 'Doe', 'Developer', 3000);
+// const emp2 = new Employee('Barbara', 'Johnson', 'QA', 2500);
+// company.addEmployee(emp1);
+// company.addEmployee(emp2);
+// console.log(company.getEmployees()); // [Employee, Employee]
+
+
+
+
 
 export { Employee, Company };
