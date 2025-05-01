9671197f0cd19b26a3782db4f73e05c03ec6fe7e
Aleksandr <a.garnov@paysenger.com>
2025-05-01 18:01:52 +0400

    add 2 task

--- homework-solutions/js/hw-12/task-2.mjs
+++ homework-solutions/js/hw-12/task-2.mjs
@@ -6,9 +6,33 @@
    Функция должна возвращать полученный объект из респонса
    Обрабатывайте ошибки с помощью try/cath, в конце выведите в консоль текст, что работа функции завершена
    */
-
+const API_URL = 'https://jsonplaceholder.typicode.com/todos'
    async function createTodo(body) {
     try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
+        const response = await fetch(API_URL, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                "Content-type":'application/json; charset=UTF-8'
+            }
+        });
+        const data = await response.json()
+        if (response.status !== 201) {
+            throw new Error('Status is not 201')
+        }
+        if (data.title !== body.title) {
+            throw new Error('Title field is not matched with the body')
+        }
+        if (data.completed !== body.completed) {
+            throw new Error('Completed field is not matched with the body')
+        }
+        if (data.userId !== body.userId) {
+            throw new Error('UserId field is not matched with the body')
+        }
+    } catch (error) {
+        throw error
     }
-   }
+        await console.log('Function is finished')
+}
+
+createTodo({title: 'New Todo', completed: false, userId: 1})
